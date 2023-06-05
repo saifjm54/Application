24a3fc1638288ebfc06924e27ef99e978e650dd4
saifjm54 <saifjmaa54@gmail.com>
2023-06-05 23:52:33 +0100

    version 0.0.1

---
+++ src/main/java/com/example/app/controller/ProductServiceController.java
@@ -0,0 +1,38 @@
+import com.example.app.model.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ProductServiceController {
+    private static Map<String, Product> productRepo = new HashMap<>();
+
+    static {
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Honey");
+        productRepo.put(honey.getId(), honey);
+
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProduct() {
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+    }
+}
